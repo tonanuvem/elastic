3f8bb2b5c652d57e672ed96fa26adafd4809af70
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-28 17:24:54 -0300

    Create log_upload.sh

---
+++ files/log_upload.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+## To create with security disabled remove -u username:password to curl commands
+
+HOST='localhost'
+PORT=9200
+INDEX_NAME='ufw_logs'
+URL="http://${HOST}:${PORT}"
+USERNAME=elastic
+PASSWORD=changeme
+printf "\n== Script for creating index and uploading data == \n \n"
+printf "\n== Deleting old index == \n\n"
+curl -s -u ${USERNAME}:${PASSWORD} -X DELETE ${URL}/${INDEX_NAME}
+
+printf "\n== Creating Index - ${INDEX_NAME} == \n\n"
+curl -s  -u ${USERNAME}:${PASSWORD} -X PUT -H 'Content-Type: application/json' ${URL}/${INDEX_NAME} -d '{  
+  "settings":{  
+      "number_of_shards":1,
+      "number_of_replicas":0
+  },
+  "properties": {
+    "@timestamp": {
+      "type": "date"
+    },
+    "action": {
+      "type": "keyword"
+    },
+    "destination_ip": {
+      "type": "ip"
+    },
+    "destination_port": {
+      "type": "long"
+    },
+    "in": {
+      "type": "keyword"
+    },
+    "logsource": {
+      "type": "keyword"
+    },
+    "mac": {
+      "type": "keyword"
+    },
+    "message": {
+      "type": "text"
+    },
+    "out": {
+      "type": "keyword"
+    },
+    "program": {
+      "type": "keyword"
+    },
+    "protocol": {
+      "type": "keyword"
+    },
+    "source_ip": {
+      "type": "ip"
+    },
+    "source_port": {
+      "type": "long"
+    },
+    "geoip": {
+      "properties": {
+        "location": { "type": "geo_point" }
+      }
+    }
+  }
+}'
+
+curl -s -u ${USERNAME}:${PASSWORD} -X POST -H "Content-Type: application/json" ${URL}/${INDEX_NAME}/ --data-binary "ufw.log" > /dev/null
+
+#printf "\n== Bulk uploading data to index... \n"
+#for i in `seq 1 20`;
+#do
+#    curl -s -u ${USERNAME}:${PASSWORD} -X POST -H "Content-Type: application/json" ${URL}/${INDEX_NAME}/_bulk --data-binary "@server-metrics_${i}.json" > /dev/null
+#    printf "\nServer-metrics_${i} uploaded"
+#done
+
+#printf "\n done - output to /dev/null"
+
+printf "\n\n== Check upload \n"
+curl -s -u ${USERNAME}:${PASSWORD} -X GET ${URL}/_cat/indices/${INDEX_NAME}?v
+
+printf "\n Log uploaded \n "
