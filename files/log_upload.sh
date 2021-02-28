fcbeacb46e54c2f5bf5be96eeffb145fa2420164
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-28 17:51:49 -0300

    Update log_upload.sh

--- files/log_upload.sh
+++ files/log_upload.sh
@@ -5,6 +5,7 @@
 HOST='localhost'
 PORT=9200
 INDEX_NAME='ufw_logs'
+LOG_FILE='ufw.log'
 URL="http://${HOST}:${PORT}"
 USERNAME=elastic
 PASSWORD=changeme
@@ -18,55 +19,153 @@ curl -s  -u ${USERNAME}:${PASSWORD} -X PUT -H 'Content-Type: application/json' $
       "number_of_shards":1,
       "number_of_replicas":0
   },
-  "properties": {
-    "@timestamp": {
-      "type": "date"
-    },
-    "action": {
-      "type": "keyword"
-    },
-    "destination_ip": {
-      "type": "ip"
-    },
-    "destination_port": {
-      "type": "long"
-    },
-    "in": {
-      "type": "keyword"
-    },
-    "logsource": {
-      "type": "keyword"
-    },
-    "mac": {
-      "type": "keyword"
-    },
-    "message": {
-      "type": "text"
-    },
-    "out": {
-      "type": "keyword"
-    },
-    "program": {
-      "type": "keyword"
-    },
-    "protocol": {
-      "type": "keyword"
-    },
-    "source_ip": {
-      "type": "ip"
-    },
-    "source_port": {
-      "type": "long"
-    },
-    "geoip": {
+  "mappings": {
+    "_doc": {
       "properties": {
-        "location": { "type": "geo_point" }
+        "@timestamp": {
+          "type": "date"
+        },
+        "action": {
+          "type": "keyword"
+        },
+        "destination_ip": {
+          "type": "ip"
+        },
+        "destination_port": {
+          "type": "long"
+        },
+        "geoip": {
+          "properties": {
+            "city_name": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            },
+            "continent_name": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            },
+            "country_iso_code": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            },
+            "country_name": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            },
+            "location": {
+              "type": "geo_point"
+            },
+            "region_iso_code": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            },
+            "region_name": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            }
+          }
+        },
+        "in": {
+          "type": "keyword"
+        },
+        "logsource": {
+          "type": "keyword"
+        },
+        "mac": {
+          "type": "keyword"
+        },
+        "message": {
+          "type": "text"
+        },
+        "out": {
+          "type": "keyword"
+        },
+        "program": {
+          "type": "keyword"
+        },
+        "protocol": {
+          "type": "keyword"
+        },
+        "source_ip": {
+          "type": "ip"
+        },
+        "source_port": {
+          "type": "long"
+        }
       }
     }
   }
 }'
 
-curl -s -u ${USERNAME}:${PASSWORD} -X POST -H "Content-Type: application/json" ${URL}/${INDEX_NAME}/ --data-binary "ufw.log" > /dev/null
+# curl -s  -u ${USERNAME}:${PASSWORD} -X PUT -H 'Content-Type: application/json' ${URL}/
+curl -X PUT "localhost:9200/_ingest/pipeline/${INDEX_NAME}-pipeline?pretty" -H 'Content-Type: application/json' -d'
+{
+  "description" : "ufw_logs pipeline",
+  "processors" : [
+  {
+    "grok": {
+      "field": "message",
+      "patterns": [
+        "%{SYSLOGBASE} \\[%{DATA}\\] \\[%{DATA:action}\\] IN=(%{WORD:in})? OUT=(%{WORD:out})?( MAC=%{DATA:mac})? SRC=%{IP:source_ip} DST=%{IP:destination_ip} %{DATA} PROTO=%{WORD:protocol}( SPT=%{INT:source_port} DPT=%{INT:destination_port})?"
+      ]
+    }
+  },
+  {
+    "date": {
+      "field": "timestamp",
+      "timezone": "America/Sao_Paulo",
+      "formats": [
+        "MMM dd HH:mm:ss",
+        "MMM  d HH:mm:ss",
+        "MMM d HH:mm:ss"
+      ]
+    }
+  },
+  {
+    "remove": {
+      "field": "timestamp"
+    }
+  },
+  {
+    "geoip": {
+      "field": "source_ip"
+    }
+  }
+  ]
+}'
+
+
+curl -s -u ${USERNAME}:${PASSWORD} -X POST -H "Content-Type: application/json" ${URL}/${INDEX_NAME}/ --data-binary ${LOG_FILE} > /dev/null
 
 #printf "\n== Bulk uploading data to index... \n"
 #for i in `seq 1 20`;
