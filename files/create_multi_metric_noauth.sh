a9e2895bbc98bc06dfc2aba244829e709882ed99
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-28 09:21:26 -0300

    Add files via upload

---
+++ files/create_multi_metric_noauth.sh
@@ -0,0 +1,73 @@
+HOST='localhost'
+PORT=9200
+JOB_ID='scripted_response_request_partition-app'
+INDEX_NAME='server-metrics'
+ROOT="http://${HOST}:${PORT}/_xpack/ml"
+JOBS="${ROOT}/anomaly_detectors"
+DATAFEEDS="${ROOT}/datafeeds"
+printf "\n== Script started for... $JOBS/$JOB_ID"
+
+printf "\n\n== Stopping datafeed... "
+curl -s -X POST ${DATAFEEDS}/datafeed-${JOB_ID}/_stop
+
+printf "\n\n== Deleting datafeed... "
+curl -s -X DELETE ${DATAFEEDS}/datafeed-${JOB_ID}
+
+printf "\n\n== Closing job... "
+curl -s -X POST ${JOBS}/${JOB_ID}/_close
+
+printf "\n\n== Deleting job... "
+curl -s -X DELETE ${JOBS}/${JOB_ID}
+
+printf "\n\n== Creating job... \n"
+curl -s -X PUT -H 'Content-Type: application/json' ${JOBS}/${JOB_ID}?pretty -d '{
+    "description" : "Unusual service behaviour",
+    "analysis_config" : {
+        "bucket_span":"10m",
+        "detectors" :[
+          {
+            "detector_description": "Unusual sum for each  service",
+            "function": "sum",
+            "field_name": "total",
+            "partition_field_name": "service"
+          },
+          {
+            "detector_description": "Unusually high response for each service",
+            "function": "high_mean",
+            "field_name": "response",
+            "partition_field_name": "service"
+          }
+        ],
+        "influencers": ["host"]
+        },
+        "data_description" : {
+          "time_field":"@timestamp",
+          "time_format": "epoch_ms"
+    }
+}'
+printf "\n\n== Creating Datafeed... \n"
+curl -s -X PUT -H 'Content-Type: application/json' ${DATAFEEDS}/datafeed-${JOB_ID}?pretty -d '{
+      "job_id" : "'"$JOB_ID"'",
+      "indexes" : [
+        "'"$INDEX_NAME"'"
+      ],
+      "types" : [
+        "metric"
+      ],
+      "scroll_size" : 1000
+}'
+printf "\n\n== Opening job for ${JOB_ID}... "
+curl -X POST ${JOBS}/${JOB_ID}/_open
+
+printf "\n\n== Starting datafeed-${JOB_ID}... "
+curl -X POST "${DATAFEEDS}/datafeed-${JOB_ID}/_start?start=1970-01-02T10:00:00Z&end=2017-06-01T00:00:00Z"
+
+sleep 20s
+
+printf "\n\n== Stopping datafeed-${JOB_ID}... "
+curl -X POST "${DATAFEEDS}/datafeed-${JOB_ID}/_stop"
+
+printf "\n\n== Closing job for ${JOB_ID}... "
+curl -X POST ${JOBS}/${JOB_ID}/_close
+
+printf "\n\n== Finished ==\n\n"
---
+++ files/upload_server_metrics_noauth.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+HOST='localhost'
+PORT=9200
+INDEX_NAME='server-metrics'
+URL="http://${HOST}:${PORT}"
+printf "\n== Script for creating index and uploading data == \n \n"
+printf "\n== Deleting old index == \n\n"
+curl -s -X DELETE ${URL}/${INDEX_NAME}
+
+printf "\n== Creating Index - ${INDEX_NAME} == \n\n"
+curl -s -X PUT -H 'Content-Type: application/json' ${URL}/${INDEX_NAME} -d '{  
+   "settings":{  
+      "number_of_shards":1,
+      "number_of_replicas":0
+   },
+   "mappings":{  
+      "metric":{  
+         "properties":{  
+            "@timestamp":{  
+               "type":"date"
+            },
+            "accept":{  
+               "type":"long"
+            },
+            "deny":{  
+               "type":"long"
+            },
+            "host":{  
+               "type":"keyword"
+            },
+            "response":{  
+               "type":"float"
+            },
+            "service":{  
+               "type":"keyword"
+            },
+            "total":{  
+               "type":"long"
+            }
+         }
+      }
+   }
+}'
+
+printf "\n== Bulk uploading data to index... \n"
+for i in `seq 1 20`;
+do
+    curl -s -X POST -H "Content-Type: application/json" ${URL}/${INDEX_NAME}/_bulk --data-binary "@server-metrics_${i}.json" > /dev/null 
+    
+    printf "\nServer-metrics_${i} uploaded"
+done
+
+printf "\n done - output to /dev/null"
+
+printf "\n\n== Check upload \n"
+curl -s -X GET ${URL}/_cat/indices/${INDEX_NAME}?v
+
+printf "\n Server-metrics uploaded \n "
