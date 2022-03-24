4aeafaafdaac9b5b99e3c5e1492e6bc1a433a920
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-03-24 13:31:42 -0300

    Update server.js

--- app-client-nodejs-apm/server.js
+++ app-client-nodejs-apm/server.js
@@ -23,7 +23,7 @@ const logger = winston.createLogger({
   level: 'debug',
   format: ecsFormat({ convertReqRes: true }),
   transports: [
-    //new winston.transports.Console(),
+    new winston.transports.Console(),
     new winston.transports.File({
       //path to log file
       filename: 'logs/log.json',
