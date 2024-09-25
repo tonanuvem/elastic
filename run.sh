54a52b4f97cf3f1ac8f4bd376be4ee3296c8354b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-09-25 09:24:38 -0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Executando Elastic stack"
+echo ""
+echo ""
+
+# Start up all services
+docker-compose up -d
+
+IP=$(curl checkip.amazonaws.com)
+
+echo ""
+echo "URL de acesso: Aplicação Node.js"
+echo ""
+echo http://$(curl -s checkip.amazonaws.com):3000
+echo ""
+echo ""
+echo ""
+echo "URL de acesso: Kibana"
+echo ""
+echo http://$(curl -s checkip.amazonaws.com):5601
+echo ""
+echo ""
