6c1a1da274680918673f76ce6ea2ca076ad84d92
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-02-11 11:38:37 -0300

    Refine echo statements in run.sh
    Updated echo statements for clarity and removed redundant text.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Executando Elastic stack para análise dos dados"
+echo "Executando Elastic stack"
 echo ""
 echo ""
 
@@ -9,6 +9,14 @@ docker-compose up -d
 
 IP=$(curl checkip.amazonaws.com)
 
+echo ""
+echo "--------------------"
+echo ""
+echo "URL de acesso: Aplicacao-fiap-nodejs-v2"
+echo ""
+echo http://$(curl -s checkip.amazonaws.com):3001
+echo ""
+echo ""
 echo ""
 echo "URL de acesso: Kibana"
 echo ""
